5b2653933402495d704f6312f7e4b6615f5da8a3
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2023-08-17 06:42:19 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// // ------------------------------------ Typescript Test
+// // console.log("Create a Map Function for Objects");
+// const MyBioObject:
+//     {
+//         name: string,
+//         father_name: string,
+//         test_name: string,
+//         age: number,
+//          height: number,
+//         gender: string,
+//         test: boolean
+//     } = {
+//     name: "Rana Rabees",
+//     father_name: "Rana Amir",
+//     test_name: "Typescript",
+//     age: 13,
+//     height: 180,
+//     gender: "male",
+//     test: true
+// };
+// const myBiomap = new Map(Object.entries(MyBioObject));
+// console.log(myBiomap);
+function multiply(howmanystars) {
+    var stercs = "";
+    for (var i = howmanystars; i >= 1; i--) {
+        for (var s = 0; s < i; s++) {
+            stercs += "* ";
+        }
+        stercs += "\n";
+    }
+    console.log(stercs);
+    alert("Your Multiply chart is below" + stercs);
+    // document.write(stercs);
+    return stercs;
+}
+multiply(7);
